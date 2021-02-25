4030a7a92e24aed8051c2248a33571a570adecf7
thom974 <74675902+thom974@users.noreply.github.com>
2021-02-25 11:35:13 -0500

    work day one - setting things up
    -found basic word list (?)
    -setup most needed files for now

---
+++ main.py
@@ -0,0 +1,17 @@
+# IMPORTS --------------------------
+import pygame
+import scripts.menus as m
+# INIT -----------------------------
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+running = True
+# MAIN GAME LOOP -------------------
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((255,255,255))
+    m.draw_circle(screen,10)
+    pygame.display.flip()
+
---
+++ scripts/menus.py
@@ -0,0 +1,9 @@
+# IMPORTS --------------------------
+import pygame
+# INIT -----------------------------
+pygame.init()
+# FUNCTIONS ------------------------
+def draw_circle(screen,r):
+    pygame.draw.circle(screen,(255,0,0),(400,300),r)
+
+
