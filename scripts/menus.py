19ed59e6a630da37bc4e7efa30063e1c01677c1d
thom974 <74675902+thom974@users.noreply.github.com>
2021-03-02 19:59:00 -0500

    added transition

--- scripts/menus.py
+++ scripts/menus.py
@@ -10,7 +10,23 @@ spreadsheet.fetch_endpoint()
 title_font = pygame.font.Font("data/fonts/scribble.ttf",80)
 game_font = pygame.font.Font("data/fonts/scribble.ttf",30)
 FPS = 60
+transition_dictionary = {}
 # FUNCTIONS ------------------------
+def load_animation_sequence(lst):
+    transition_dictionary['scribble'] = []
+    for i in range(1,len(lst)+1):
+        for _ in range(lst[i-1]):
+            transition_dictionary['scribble'].append("transition" + str(i))
+    print(transition_dictionary['scribble'])
+
+def loading_trans_dictionary():
+    for i in range(1, 7):
+        name = 'transition' + str(i)
+        surf = pygame.image.load("data/images/transition/" + name + ".png").convert()
+        surf.set_colorkey((0,0,0))
+        transition_dictionary[name] = surf
+    print(transition_dictionary)
+
 def loading_font(string, colour,position,*args):  # string = text to display, colour = tuple, position = tuple
     if len(args) > 0 and args[0]:
         font = title_font.render(string, True, colour)
@@ -19,13 +35,25 @@ def loading_font(string, colour,position,*args):  # string = text to display, co
         font = game_font.render(string,True,colour)
         font_rect = font.get_rect(center=position)
     return [font, font_rect]
+
+def transition(screen,frame):
+    current_frame_name = transition_dictionary['scribble'][frame]
+    current_frame_surf = transition_dictionary[current_frame_name]
+    screen.blit(current_frame_surf,(0,0))
+
 # MENUS ----------------------------
 def game_mode_one(screen):
     # game mode 1 class
     mode = c.G1(spreadsheet)
     mode.start()
+
+    # transition variables
+    start_transition = False
+    transition_frame = 0
+
     # main loop variables + control
     running = True
+    clock = pygame.time.Clock()
     while running:
         # clear screen
         screen.fill((255,0,0))
@@ -35,13 +63,23 @@ def game_mode_one(screen):
                 exit()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
-                    running = False
+                    start_transition = True
+
+        # transition code
+        if start_transition:
+            transition(screen,transition_frame)
+            if transition_frame < len(transition_dictionary['scribble']) - 1:
+                transition_frame += 1
+            else:
+                running = False
 
         pygame.display.flip()
+        clock.tick(FPS)
 
 def game_mode_two(screen):
     # main loop variables + control
     running = True
+    clock = pygame.time.Clock()
     while running:
         # clear screen
         screen.fill((0, 255, 0))
@@ -54,10 +92,12 @@ def game_mode_two(screen):
                     running = False
 
         pygame.display.flip()
+        clock.tick(FPS)
 
 def game_mode_three(screen):
     # main loop variables + control
     running = True
+    clock = pygame.time.Clock()
     while running:
         # clear screen
         screen.fill((0, 0, 255))
@@ -70,9 +110,16 @@ def game_mode_three(screen):
                     running = False
 
         pygame.display.flip()
+        clock.tick(FPS)
 
 # main menu - what will be seen upon program launch
 def main_menu(screen):
+    # load transition dictionary
+    loading_trans_dictionary()
+    load_animation_sequence([5,5,5,5,5,5])
+    start_transition = False
+    transition_frame = 0
+
     # creating variables
     title, title_rect = loading_font("The Reading Corner",(255,0,0),(400,150),True)
     mode_one, mode_one_rect = loading_font("Single Word Mode",(222, 29, 80),(240,300))
@@ -103,12 +150,7 @@ def main_menu(screen):
                 if event.key == pygame.K_UP:
                     selector.change_mode(-1)
                 if event.key == pygame.K_RETURN:
-                    if selector.current_mode == 0:
-                        game_mode_one(screen)
-                    elif selector.current_mode == 1:
-                        game_mode_two(screen)
-                    elif selector.current_mode == 2:
-                        game_mode_three(screen)
+                    start_transition = True
 
         # selector code
         selector.draw_frame()
@@ -119,6 +161,21 @@ def main_menu(screen):
         screen.blit(mode_one,mode_one_rect)
         screen.blit(mode_two,mode_two_rect)
         screen.blit(mode_three, mode_three_rect)
+
+        # transition code
+        if start_transition:
+            transition(screen,transition_frame)
+            if transition_frame < len(transition_dictionary['scribble']) - 1:
+                transition_frame += 1
+            else:
+                start_transition = False
+                transition_frame = 0
+                if selector.current_mode == 0:
+                    game_mode_one(screen)
+                elif selector.current_mode == 1:
+                    game_mode_two(screen)
+                elif selector.current_mode == 2:
+                    game_mode_three(screen)
 
         # pygame code
         pygame.display.flip()
